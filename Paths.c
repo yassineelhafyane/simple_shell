69a2f9b276af3801b8a95d8c518c64d89538aedd
Mohamed <developperw@gmail.com>
2023-09-21 00:11:33 +0100

    Update Paths.c

--- Paths.c
+++ Paths.c
@@ -14,18 +14,18 @@ int path_cmd(char **cmd)
 	value = _strtok(path, ":");
 	while (value != NULL)
 	{
-		path_cmd = build(*cmd, value);
-		if (stat(path_cmd, &buf) == 0)
+		cmd_path = build(*cmd, value);
+		if (stat(cmd_path, &buf) == 0)
 		{
-			*cmd = _strdup(path_cmd);
-			free(path_cmd);
-			free(cmd);
+			*cmd = _strdup(cmd_path);
+			free(cmd_path);
+			free(path);
 			return (0);
 		}
-		free(path_cmd);
+		free(cmd_path);
 		value = _strtok(NULL, ":");
 	}
-	free(cmd);
+	free(path);
 
 	return (1);
 }
